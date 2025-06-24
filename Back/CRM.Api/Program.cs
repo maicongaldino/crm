43dc9d8083b7c92b2700e4ad86fdc675b79f6c4d
maicongaldino <maicongaldino460@gmail.com>
2025-06-24 04:13:38 -0300

    🐛 fix: mover chamada de AddInfrastructure para antes do Build no Program.cs para evitar erro de IServiceCollection readonly

--- Back/CRM.Api/Program.cs
+++ Back/CRM.Api/Program.cs
@@ -1,9 +1,10 @@
 using CRM.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
 
 builder.Services.AddInfrastructure(builder.Configuration);
+
+var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
